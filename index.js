60456274a0aed04455e64a87fcdbef659af99cbc
Eva Navarro <emanao@me.com>
2018-06-01 20:32:39 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 function nestedTarget(){
-  return document.querySelector("div#nested div.target").innerHTML;
+  return document.querySelector("div#nested div.target");
+}
+function increaseRankBy(n){
+  const rl = document.querySelectorAll("ul.ranked-list li")
+  for(let i=0; i<rl.length; ++i){
+    rl[i].innerHTML = parseInt(rl[i].innerHTML, 10) + n ;
+  }
 }
